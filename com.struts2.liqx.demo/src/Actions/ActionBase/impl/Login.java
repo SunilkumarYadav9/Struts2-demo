18dcfbefce0661f7c6685c0b242a0e63d900467a
lqx0830 <lqx0830@hotmail.com>
2012-03-08 14:52:16 +0800

    add log output

--- com.struts2.liqx.demo/src/Actions/ActionBase/impl/Login.java
+++ com.struts2.liqx.demo/src/Actions/ActionBase/impl/Login.java
@@ -22,6 +22,7 @@ public class Login implements ActionBase {
 	private String username = "";
 	private String password = "";
 	private String[] books = new String[] { "1111", "2222", "3333" };
+	private static Logger log = LoggerFactory.getLogger(Login.class);
 
 	public String[] getBooks() {
 		return books;
@@ -31,7 +32,6 @@ public class Login implements ActionBase {
 		this.books = books;
 	}
 
-	private static Logger log = LoggerFactory.getLogger(Login.class);
 
 	public String getUsername() {
 		return username;
@@ -54,17 +54,22 @@ public class Login implements ActionBase {
 		log.debug(getUsername());
 		HibernateUtil hbUtil = new HibernateUtil();
 		Session sn =hbUtil.getSession();
-
+		log.info("login");
 		User user = UserDao.getUser(1);
+		if(null != user){
+			log.info("username{0},pwd={1}",user.getUserName(),user.getPwd());
+		}else{log.info("no user.");System.out.println("null");}
 		try {
-			if (user.getUserName().equals("admin")
-					&& user.getPwd().equals("admin")) {
+			if (user.getUserName().equals("1")
+					&& user.getPwd().equals("1")) {
 				// put user name to the view
 				ActionContext.getContext().getSession().put("user",
 						getUsername());
 				ActionContext.getContext().getSession()
 						.put("books", getBooks());
 				log.info("login");
+				log.info("username{0},pwd={1}",user.getUserName(),user.getPwd());
+				System.out.println("username="+user.getUserName()+"pwd="+user.getPwd());
 				return SUCCESS;
 			}
 		} catch (HibernateException he) {
